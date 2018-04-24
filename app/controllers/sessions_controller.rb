47aae74dd80e714070eb96a9350efb97688f8b18
jonathan senior <jonosenior@gmail.com>
2018-04-24 12:32:03 +0200

    Add basic login without password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:session][:name].downcase)
+    user = User.find_by(name: params[:session][:name])
     if user
       login user
-      redirect_to root_path
+      redirect_to user
     else
       render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  root 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create, :show]
 end
