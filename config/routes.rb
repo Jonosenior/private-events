baae00439a47ab907cb49a81ff7f27c420ad9e83
jonathan senior <jonosenior@gmail.com>
2018-04-24 14:21:28 +0200

    Add events controller and actions for new, create and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   root 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+  resources :events, only: [:new, :create, :show, :index]
   resources :users, only: [:new, :create, :show]
 end
