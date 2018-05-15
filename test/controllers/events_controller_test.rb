8fcc41ff78d7dda5226acfc9ec749233c190d2c6
jonathan senior <jonosenior@gmail.com>
2018-05-15 13:25:45 +0200

    Add (as yet) still failing Events Controller index test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,24 +1,28 @@
 require 'test_helper'
 
-# class EventsControllerTest < ActionDispatch::IntegrationTest
-#   test "should get new" do
-#     get events_new_url
-#     assert_response :success
-#   end
-#
-#   test "should get create" do
-#     get events_create_url
-#     assert_response :success
-#   end
-#
-#   test "should get show" do
-#     get events_show_url
-#     assert_response :success
-#   end
-#
-#   test "should get index" do
-#     get events_index_url
-#     assert_response :success
-#   end
-#
-# end
+class EventsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @event = users(:valid).created_events.create(name: "25th Birthday Party", description: "Some lorem ipsum text about the party.", date_time: "2018-09-24 12:45:00", location: "Repeater Bar")
+  end
+
+  test 'index action should be success' do
+    get events_path
+    assert_template 'index'
+  end
+
+  # test "should get create" do
+  #   get events_create_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get show" do
+  #   get events_show_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get index" do
+  #   get events_index_url
+  #   assert_response :success
+  # end
+  #
+end
