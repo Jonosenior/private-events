7a1a7a94b66e99b056768a61e8811c8a33e3aa1a
jonathan senior <jonosenior@gmail.com>
2018-05-15 10:36:46 +0200

    Add tests for attendance

--- test/models/attendance_test.rb
+++ test/models/attendance_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class AttendanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @attendance = Attendance.new(user_id: User.first.id, event_id: Event.first.id)
+  end
+
+  test 'valid attendance' do
+    assert @attendance.valid?
+  end
+
+  test 'user must be present' do
+    @attendance.user_id = nil
+    refute @attendance.valid?
+  end
+
+  test 'event must be present' do
+    @attendance.event_id = nil
+    refute @attendance.valid?
+  end
+
 end
