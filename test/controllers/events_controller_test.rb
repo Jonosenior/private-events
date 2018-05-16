facb6b9cd1c28e43ebc45b81b275ed4b067923d5
jonathan senior <jonosenior@gmail.com>
2018-05-16 13:30:12 +0200

    Solved Events Controller test bug

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class EventsControllerTest < ActionDispatch::IntegrationTest
   def setup
     # @event = users(:valid).created_events.create(name: "25th Birthday Party", description: "Some lorem ipsum text about the party.", date_time: "2018-09-24 12:45:00", location: "Repeater Bar")
+    @user = users(:valid)
     @event = events(:valid)
   end
 
@@ -12,19 +13,20 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should get create" do
-  #   get events_create_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get events_show_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get index" do
-  #   get events_index_url
-  #   assert_response :success
-  # end
-  #
+  test "should redirect new when not logged in" do
+    get new_event_path
+    assert_redirected_to login_url
+  end
+
+  test "should get new when logged in" do
+    login(@user)
+    get new_event_path
+    assert_response :success
+  end
+
+  test "should get show" do
+    get event_path(@event.id)
+    assert_response :success
+  end
+
 end
